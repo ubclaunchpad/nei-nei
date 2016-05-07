d60bdd1f2c30a76038f506f97a44276e0401bf2c
Steven Wallace <steven@thewallaces.ca>
2016-05-07 13:11:12 -0700

    Django project created

--- rentmyrez/rentmyrez/urls.py
+++ rentmyrez/rentmyrez/urls.py
@@ -1,5 +1,21 @@
-from listings.views import ListingViewSet
+"""rentmyrez URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.contrib import admin
 from django.conf.urls import patterns, url, include
+from listings.views import ListingViewSet
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 
@@ -8,6 +24,6 @@ router.register(r'listings', ListingViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', views.obtain_auth_token)
-]
