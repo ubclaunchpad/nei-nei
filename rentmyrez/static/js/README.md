f1d52b28896401a8280fc14469a0d3db9ee73c23
Jordan Schalm <jordan.schalm@gmail.com>
2016-05-07 19:45:26 -0700

    Set up React build process and development environment. Update gitignore for node/js stuff.

---
+++ rentmyrez/static/js/README.md
@@ -0,0 +1,8 @@
+# Rent My Rez
+
+Instructions for setting up the development environment.
+
+1. Install Node dependencies. `npm install`
+2. Install the development server. `npm install -g webpack-dev-server`
+3. Start the development server. `webpack-dev-server`
+4. You're ready to start creating React components! The server will automatically watch your files and recompile.
---
+++ rentmyrez/static/js/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	entry: './App.js',
+	output: {
+		path: __dirname,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: [
+						'es2015',
+						'react'
+					],
+					plugins: [
+						'transform-class-properties'
+					]
+				}
+			}
+		]
+	}
+};
