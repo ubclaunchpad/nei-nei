45cbfc460c8cf670da8088c6e83b45a43a6533ce
Steven Wallace <steven@thewallaces.ca>
2016-05-19 22:23:42 -0700

    initialzed app

--- rentmyrez/rentmyrez/settings.py
+++ rentmyrez/rentmyrez/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'housing_heatmap',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'rentmyrez.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rmr_db',
+        'USER': 'admin',
+        'PASSWORD': 'steveiscool',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
