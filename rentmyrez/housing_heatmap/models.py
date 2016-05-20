a1aef64c4135a1671f3c390639a56f79b5e37d5a
Steven Wallace <steven@thewallaces.ca>
2016-05-19 22:34:10 -0700

    House model made

--- rentmyrez/housing_heatmap/models.py
+++ rentmyrez/housing_heatmap/models.py
@@ -1,5 +1,16 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import uuid
 
-# Create your models here.
+class House(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    num_bedrooms = models.IntegerField()
+    square_footage = models.IntegerField()
+    rent = models.IntegerField()
+    posted = models.DateField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __unicode__(self):
+        return self.uuid
