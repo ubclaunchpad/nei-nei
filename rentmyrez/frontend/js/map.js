e5dd45978c3235bb61e0624f3313557f669039e6
Elaine Feng <eleanor52@live.ca>
2016-05-21 16:30:44 -0700

    Map with circle markers

--- rentmyrez/frontend/js/map.js
+++ rentmyrez/frontend/js/map.js
@@ -1,9 +1,43 @@
+var map;
+
 function initMap() {
 	var mapDiv = document.getElementById('map');
-	var map = new google.maps.Map(mapDiv, {
+	map = new google.maps.Map(mapDiv, {
 		center: {lat: 44.540, lng: -78.546},
-		zoom: 8
+		zoom: 8,
+		mapTypeId: google.maps.MapTypeId.TERRAIN
 	});
+
+	// Create a <script> tag and set the USGS URL as the source.
+	var script = document.createElement('script');
+
+	script.src = 'http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp';
+	document.getElementsByTagName('head')[0].appendChild(script);
+
+	map.data.setStyle(function(feature) {
+		console.log('feature', feature);
+		var magnitude = feature.getProperty('mag');
+		console.log('magnitude', magnitude);
+		return {
+			icon: getCircle(magnitude)
+		};
+	});
+}
+
+function getCircle(magnitude) {
+  var circle = {
+    path: google.maps.SymbolPath.CIRCLE,
+    fillColor: 'red',
+    fillOpacity: .2,
+    scale: Math.pow(2, magnitude) / 2,
+    strokeColor: 'white',
+    strokeWeight: .5
+  };
+  return circle;
+}
+
+window.eqfeed_callback = function (results) {
+  map.data.addGeoJson(results);
 }
 
 function getPostings (cb) {
