564bf993fd6b8b1f3c936fdab7701ee9b4808cb1
Eric Stroczynski <ericstroczynski@gmail.com>
2016-06-04 18:14:25 -0700

    added data extraction fields

--- rentmyrez/scraper/RMRSpider/RMRSpider/spiders/rmrSpider.py
+++ rentmyrez/scraper/RMRSpider/RMRSpider/spiders/rmrSpider.py
@@ -1,15 +1,43 @@
 from scrapy.contrib.spiders import CrawlSpider, Rule
-from RMRSpider.items import RmrspiderItem
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
+from RMRSpider.items import RmrSpiderItem
+from selenium import webdriver
+
+# Selenium and PhantomJS needed to execute JavaScript and render AJAX-enabled dynamic pages
+pjs = webdriver.PhantomJS()
+pjs.get("https://www.webpage.com")
+pageSource = pjs.page_source
 
 class RMRSpider(CrawlSpider):
-    name = "post"
-    allowed_domains = ["vancouver.craigslist.ca"]
-    start_urls = ["https://vancouver.craigslist.ca/search/apa"]
-    rules = [Rule(SgmlLinkExtractor(allow=('(/\w{1,4}/)?/apa(\?s=\d*)?/\d*.html'),), callback="parse_item", follow=True)]
+    name = "rmrspider"
+    allowed_domains = ["https://www.padmapper.com", "https://www.airbnb.com/rooms"]
+    start_urls = ["https://www.padmapper.com/?viewType=LIST&lat=49.222899&lng=-123.038579&zoom=10&minRent=100&maxRent=5000&minBR=0&maxBR=10&minBA=1&cats=false&dogs=false"]
+    rules = (
+        # AirBnB
+        Rule(SgmlLinkExtractor(allow=('(/rooms/[\d\w\?\-\=]*)'), ), callback="parse_bnb", follow=True),
 
-    def parse_item(self, response):
-        item = RmrspiderItem()
-        title = response.xpath('//h1/text()')[0].extract()
-        item['title'] = title
+        # Padmapper (posts on site)
+        Rule(SgmlLinkExtractor(allow=('(/listings/[\d\w\.\-\=]*)'), ), callback="parse_pm", follow=True)
+    )
+
+    # AirBnB-specific parser
+    def parse_bnb(self, response):
+        # Define spider item, created by extracting data from HTML tags
+        item = RmrSpiderItem()
+
+        # Data selectors
+        item['latitude']  = response.xpath('//meta[@property="airbedandbreakfast:location:latitude"]/@content/text()').extract()
+        item['longitude'] = response.xpath('//meta[@property="airbedandbreakfast:location:longitude"]/@content/text()').extract()
+        return item
+
+    # Padmapper-specific parser
+    def parse_pm(self, response):
+        # Define spider item, created by extracting data from HTML tags
+        item = RmrSpiderItem()
+
+        # Data selectors
+        item['latitude']  = response.xpath('//meta[@property="place:location:latitude"]/@content/text()').extract()
+        item['longitude'] = response.xpath('//meta[@property="place:location:longitude"]/@content/text()').extract()
         return item
