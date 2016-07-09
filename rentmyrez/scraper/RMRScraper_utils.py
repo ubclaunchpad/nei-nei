352af9bd41b2821f1d2232a1f1fd082db8225db2
Eric Stroczynski <ericstroczynski@gmail.com>
2016-07-09 16:50:12 -0700

    Updated to reflect multiple map scraping

--- rentmyrez/scraper/RMRScraper_utils.py
+++ rentmyrez/scraper/RMRScraper_utils.py
@@ -1,10 +1,21 @@
 from urllib.request import urlopen
+from urllib.request import Request
 from urllib.error import HTTPError, URLError
-from urllib.parse import urlparse
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 import time
 import re
 import logging
+
+# Custom header to access AirBnB
+header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
 
 # Get immediate child of listing-preview that contains URL to actual posting
 #   Tag: a href="URL"
@@ -40,7 +51,7 @@ def getPrice(post):
 #   Ex. 1 Bed (Can be studio)
 def getNumBeds(post):
     numBeds = post.find("div", {"class":"listing-num-bedrooms"}).get_text()
-    studio = re.compile("[Studio]*?")
+    studio = re.compile("[Sstudio]*?")
     if (studio.match(numBeds)): # studios are 1 br
         numBeds = 1
     else:
@@ -48,21 +59,26 @@ def getNumBeds(post):
     return numBeds
 
 # Now to step into each link for latitudes and longitudes
-def getLatLonFromLink(URL, args):
+def getLatLonFromLink(webDriver, URL, *args):
     lat, lon = None, None
     try:
-        html = urlopen(URL)
+        time.sleep(11) # Scraper courtesy wait ~6 postings/minute
+        webDriver.get(URL)
+        html = webDriver.page_source
         bsObj = BeautifulSoup(html)
         # If the pair leads to an AirBnB page
-        if (args.match(URL)):
+        latLonTag = ""
+        if (args[0].match(URL)):
             latLonTag = "airbedandbreakfast"
         # Otherwise the pair is a padmapper page
         else:
             latLonTag = "place"
+        print(latLonTag)
         # Find latitude and longitude for a post
         # Each can be found in page metadata (super easy yay)
         lat = bsObj.find("meta", {"property":latLonTag+":location:latitude"})['content']
         lon = bsObj.find("meta", {"property":latLonTag+":location:longitude"})['content']
+
     except URLError:
         logging.basicConfig(filename="get_latlon.log", level=logging.DEBUG)
         logging.exception('getLatLonFromLink()', exc_info=True)
@@ -71,9 +87,12 @@ def getLatLonFromLink(URL, args):
 
 # Store general posting data
 # Note: add imgPath once a schema addition for images of rentals has been defined
-def addPosting(postingList, extURL, listAddr, price, numBeds):
-    airbnb = re.compile(r"http\:\/\/(www\.)?airbnb\.com\/rooms\/[\w\?\&\-\=]*")
-    lat, lon = getLatLonFromLink(extURL, airbnb)
+def addPosting(webDriver, postingList, extURL, listAddr, price, numBeds):
+    airbnb = re.compile(r"https\:\/\/(www\.)?airbnb\.com\/rooms\/[\w\?\&\-\=]*")
+    lat, lon = getLatLonFromLink(webDriver, extURL, airbnb)
+
+    if lat is None or lon is None:
+        raise TypeError
 
     post = {
         "extURL":       extURL,
@@ -84,23 +103,32 @@ def addPosting(postingList, extURL, listAddr, price, numBeds):
         "longitude":    lon
     }
     postingList.append(post)
-    print ("Added a posting from URL: " + str(extURL))
+    # print ("Added a posting from URL: " + str(extURL))
 
 # Get a posting preview div on page
 #   Tag: div class="listing-preview"
 def getPostsFromPage(bsObject, postingList):
-    posts = 0
-    fails = 0
+    driver = webdriver.PhantomJS()
+    successPosts   = 0
+    latLonFailures = 0
+    totalFailures  = 0
     for post in bsObject.findAll("div", {"class":"listing-preview"}):
         if (post.get_text() == ''): break # scraper has hit the last div so end
         try:
-            addPosting(postingList, getExtURL(post), getListAddr(post), getPrice(post), getNumBeds(post))
-            posts += 1
-        except:
-            fails += 1
+            addPosting(driver, postingList, getExtURL(post), getListAddr(post), getPrice(post), getNumBeds(post))
+            successPosts += 1
+        except TypeError:
+            latLonFailures += 1
             logging.basicConfig(filename="get_posts.log", level=logging.DEBUG)
-            logging.exception('getPostsFromPage(): addPosting()', exc_info=True)
+            logging.exception('getPostsFromPage(): addPosting(): lat or long is null', exc_info=True)
             pass
+        except:
+            totalFailures += 1
+            logging.basicConfig(filename="get_posts.log", level=logging.DEBUG)
+            logging.exception('getPostsFromPage(): addPosting(): complete failure to create post', exc_info=True)
+            pass
+    driver.quit()
 
-    print("Finished storing " + str(posts) + " listings.")
-    print("Failed " + str(fails) + " times.")
+    print("Finished storing " + str(successPosts) + " listings.")
+    print("Failed to get latitude or longitude " + str(latLonFailures) + " times.")
+    print("Failed to create posts " + str(totalFailures) + " times.")
--- rentmyrez/scraper/main.py
+++ rentmyrez/scraper/main.py
@@ -1,57 +1,84 @@
 import RMRScraper_utils as rmr
-from functools import partial
+import calc_coords as cc
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
 import time
 
+# display = Display(visible=0, size=(1024,768))
+# display.start()
+
 # Selenium and PhantomJS needed to execute JavaScript and render AJAX-enabled dynamic pages
-pjs = webdriver.PhantomJS()
-pjs.get("https://www.padmapper.com/?viewType=LIST&lat=49.273800&lng=-123.161000&zoom=12&minRent=100&maxRent=5000&minBR=0&maxBR=10&minBA=1&cats=false&dogs=false")
-# Wait until div's of the class 'listing-address' are present before grabbing source
-WebDriverWait(pjs, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'listing-address')))
-# Scrolling
-# Load all dynamic content using scrolling DOM actions
-element = pjs.find_element_by_class_name("list-container")
-scroll_height = pjs.execute_script('return document.getElementsByClassName("list-container")[0].scrollHeight')
-scroll_height_prev = 0
-# Loop to load as many postings as possible
-while(scroll_height != scroll_height_prev):
-    finalNumPosts = 0
-    pageSource = pjs.page_source
-    bsObj = BeautifulSoup(pageSource, "html.parser")
-    for post in bsObj.findAll("div", {"class": "listing-preview"}):
-        finalNumPosts += 1
-    pjs.execute_script('document.getElementsByClassName("list-container")[0].scrollTop = document.getElementsByClassName("list-container")[0].scrollHeight')
-    scroll_height_prev = scroll_height
-    scroll_height = pjs.execute_script('return document.getElementsByClassName("list-container")[0].scrollHeight')
-    print("Scroll height is " + str(scroll_height))
+driver = webdriver.PhantomJS()
 
-# Final page_source and BS objects after all scrolling complete
-finalSource = pjs.page_source
-finalBSObj  = BeautifulSoup(finalSource)
+def calculateSectorURLs(lat, lon, zoom):
+    startLat = str(lat)
+    startLon = str(lon)
+    newZoom  = str(zoom)
+    sectors = []
+    for sector in cc.mapSectors(sectors, startLat, startLon, 12):
+        nextSector = "https://www.padmapper.com/?viewType=LIST&lat="
+        + sector['latitude'] + "&lng=" + sector['longitude'] + "&zoom="
+        + newZoom + "&minRent=100&maxRent=5000&minBR=0&maxBR=10&minBA=1&cats=false&dogs=false"
+        sectors.append(sector)
+    return sectors
 
-print("Final number of posts found: "+ str(finalNumPosts))
+def scrapeSector(sector):
+    driver.get(sector)
+    # Wait until div's of the class 'listing-address' are present before grabbing source
+    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'listing-address')))
+    # Scrolling
+    # Load all dynamic content using scrolling DOM actions
+    element = driver.find_element_by_class_name("list-container")
+    scroll_height = driver.execute_script('return document.getElementsByClassName("list-container")[0].scrollHeight')
+    scroll_height_prev = 0
+    # Loop to load as many postings as possible
+    while scroll_height != scroll_height_prev:
+        finalNumPosts = 0
+        pageSource = driver.page_source
+        bsObj = BeautifulSoup(pageSource, "html.parser")
+        for post in bsObj.findAll("div", {"class": "listing-preview"}):
+            finalNumPosts += 1
+        driver.execute_script('document.getElementsByClassName("list-container")[0].scrollTop = document.getElementsByClassName("list-container")[0].scrollHeight')
+        scroll_height_prev = scroll_height
+        time.sleep(3) # Scraper courtesy wait
+        scroll_height = driver.execute_script('return document.getElementsByClassName("list-container")[0].scrollHeight')
+        print("Scroll height is " + str(scroll_height))
 
-# List of dictionaries, each describing a post, with the form:
-# [{
-#     "extURL":       extURL,
-#     "listAddr":     listAddr,
-#     "price":        price,
-#     "numBeds":      numBeds,
-#     "latitude":     latitude,
-#     "longitude":    longitude
-# }]
-postings = []
+    # Final page_source and BS objects after all scrolling complete
+    finalSource = driver.page_source
+    finalBSObj  = BeautifulSoup(finalSource)
 
-if __name__ == '__main__':
+    print("Final number of posts found: "+ str(finalNumPosts))
+
+    # List of dictionaries, each describing a post, with the form:
+    # [{
+    #     "extURL":       extURL,
+    #     "listAddr":     listAddr,
+    #     "price":        price,
+    #     "numBeds":      numBeds,
+    #     "latitude":     latitude,
+    #     "longitude":    longitude
+    # }]
+    postings = []
+
     rmr.getPostsFromPage(finalBSObj, postings)
 
-# Output file, temporary
-with open('output.json', 'w') as fout:
-    json.dump(postings, fout)
+    # Output file, temporary
+    with open('output.json', 'a') as fout:
+        json.dump(postings, fout)
 
-pjs.close()
+if __name__ == '__main__':
+    lat = 49.251756
+    lon = -123.053441
+    zoom = 12
+    sectors = calculateSectorURLs(lat, lon, zoom)
+    for sector in sectors:
+        scrapeSector(sector)
+
+driver.quit()
+# display.close()
