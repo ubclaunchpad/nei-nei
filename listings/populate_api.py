3b9b4933e74b9803aeecdc20d093f2a28edbcbc4
Simon Zhu <simon.zsiyan@gmail.com>
2016-07-09 22:57:42 -0700

    changed library to grequests, results in populate_api being faster and also no longer getting sqllite concurrency issues

--- listings/populate_api.py
+++ listings/populate_api.py
@@ -1,64 +1,30 @@
+import grequests
 import sys
 import json
 
-from StringIO import StringIO
+infile = open(sys.argv[1], 'r') if len(sys.argv) > 1 else sys.stdin
+listings = json.load(infile)
+rest_api = json.load(open('config.json'))['rest_api']
 
-from twisted.internet import reactor
-from twisted.internet import defer
-from twisted.internet import protocol
-from twisted.web.client import Agent
-from twisted.web.client import FileBodyProducer
-from twisted.web.http_headers import Headers
+def exception_handler(request, exception):
+    print exception
 
+headers = {'Accept': 'application/json',
+           'Authorization': 'Token {token}'.format(token=rest_api['token']),
+           'Content-Type': 'application/json'}
 
-def main():
-    infile = open(sys.argv[1], 'r') if len(sys.argv) > 1 else sys.stdin
-    listings = json.load(infile)
-    rest_api = json.load(open('config.json'))['rest_api']
-    success = []
+payloads = map(lambda l: dict(
+    latitude=l['lat'],
+    longitude=l['lng'],
+    bedrooms=int(l['beds']),
+    bathrooms=int(l['baths']),
+    description=l['description'],
+    listing_url=l['url'],
+    listing_id=l['id'],
+    address=l['location'],
+    price=int(l['price']),
+    date_listed=l['date']
+), listings)
 
-    def handle_response(response, id):
-        print 'Response code: {0}'.format(response.code)
-        if response.code == 201:
-            success.append(id)
-
-    def handle_failure(failure):
-        print failure.getErrorMessage()
-
-    ds = []
-
-    print 'Inserting {0} listings'.format(len(listings))
-    for listing in listings:
-        agent = Agent(reactor)
-        payload = dict(
-            latitude=listing['lat'],
-            longitude=listing['lng'],
-            bedrooms=int(listing['beds']),
-            bathrooms=int(listing['baths']),
-            description=listing['description'],
-            listing_url=listing['url'],
-            listing_id=listing['id'],
-            address=listing['location'],
-            price=int(listing['price']),
-            date_listed=listing['date']
-        )
-
-        d = agent.request(
-            'POST',
-            str(rest_api['url']),
-            Headers({'Accept': ['application/json'],
-                     'Authorization': ['Token {token}'.format(token=rest_api['token'])],
-                     'Content-Type': ['application/json']}),
-            FileBodyProducer(StringIO(json.dumps(payload))))
-        d.addCallbacks(handle_response, handle_failure, callbackArgs={'id': listing['id']})
-        ds.append(d)
-
-    dlist = defer.DeferredList(ds)
-    dlist.addCallback(lambda res : reactor.stop())
-
-    reactor.run()
-    print 'Successfully completed: {0}/{1}'.format(len(success), len(listings))
-
-
-if __name__ == '__main__':
-    main()
+rs = (grequests.post(rest_api['url'], headers=headers, data=json.dumps(payload)) for payload in payloads)
+grequests.map(rs, size=10, exception_handler=exception_handler)
