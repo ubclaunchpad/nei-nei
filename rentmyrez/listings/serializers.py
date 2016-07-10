6fd009efaad98e0271e9e0c85e8d9623fa22ecf5
Simon Zhu <simon.zsiyan@gmail.com>
2016-07-09 22:22:44 -0700

    added cron

---
+++ rentmyrez/listings/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from listings.models import Listing
+
+# TODO: save date_listed as actual date
+# change title
+class ListingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Listing
+        fields = ('url', 'latitude', 'longitude', 'price', 'date_listed',
+                  'bedrooms', 'bathrooms', 'description', 'listing_url', 'listing_id', 'address')
